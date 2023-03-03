ffa032e5b413ceef992dbed6c2288ea373b1aad5
Mchoga <mchoga1036@gmail.com>
2023-03-03 11:06:22 +0100

    xx

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello")
+import speedtest
+
+
+def test_speed():
+    st = speedtest.Speedtest()
+    download_speed = st.download() / 10 ** 6
+    upload_speed = st.upload() / 10 ** 6
+    ping = st.results.ping
+
+    print(f"Download speed: {download_speed:.2f} Mbps")
+    print(f"Upload speed: {upload_speed:.2f} Mbps")
+    print(f"Ping: {ping:.2f} ms")
+
+
+if __name__ == '__main__':
+    test_speed()
